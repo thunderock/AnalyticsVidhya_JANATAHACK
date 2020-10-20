9c47cea3f427056c5801736103c25e292f5c347b
Ashutosh Tiwari <ashutosh113163@nitp.ac.in>
2020-10-21 01:22:21 +0530

    temp

--- nlp_hackathon/threshold_optimization.py
+++ nlp_hackathon/threshold_optimization.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import pickle as pkl
 import sys
+from sklearn.metrics import f1_score
 
 
 
@@ -12,7 +13,8 @@ y = pkl.load(open("val_original.pkl", 'rb'))
 y = y[0:0 + pred.shape[0]]
 
 thresholds = [0] * 25
-
+counts = [0] * 25
+y_trues = [0] * 25
 
 def get_current_loss(p_col, true_col, th):
     p_col = sum(p_col > th)
@@ -21,10 +23,19 @@ def get_current_loss(p_col, true_col, th):
     return abs(p_col - true_col)
 
 for col in range(25):
-    current_loss = sys.maxsize
-
+    current_loss = 1
+    temp_loss = sys.maxsize
     for threshold in np.linspace(0, 1, 20, endpoint=False):
-        pred_col = np.take(pred, 0, axis=1)
-        y_col = np.take(y, 0, axis=1)
-        if get_current_loss(pred_col, y_col, threshold) < current_loss:
+        pred_col = np.take(pred, col, axis=1)
+        y_col = np.take(y, col, axis=1)
+        temp_loss = get_current_loss(pred_col, y_col, threshold)
+        if  temp_loss < current_loss:
             thresholds[col] = threshold
+            current_loss = temp_loss
+
+            counts[col] = sum(pred_col > threshold)
+            y_trues[col] = sum(y_col > threshold)
+
+print(thresholds)
+print(counts)
+print(y_trues)
