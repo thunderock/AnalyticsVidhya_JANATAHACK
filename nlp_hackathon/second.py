c1a17296e969bbc9f7a716755a0d248fb76a07e6
Ashutosh Tiwari <ashutosh113163@nitp.ac.in>
2020-10-21 15:08:40 +0530

    fixed threshold in model code

--- nlp_hackathon/second.py
+++ nlp_hackathon/second.py
@@ -18,9 +18,9 @@ logging.basicConfig(level=logging.ERROR)
 gpus = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(gpus[0], True)
 
-max_length = 400
+max_length = 600
 batch_size = 64
-epochs = 4
+epochs = 5
 
 df = pd.read_csv('Train.csv')
 
@@ -315,9 +315,10 @@ counts = [0] * 25
 y_trues = [0] * 25
 
 def get_current_loss(p_col, true_col, th):
-    p_col = sum(p_col > th)
-    true_col = sum(true_col > th)
+    p_col = sum(p_col > th) / len(p_col)
+    true_col = sum(true_col) / len(true_col)
     return abs(p_col - true_col)
+
 
 for col in range(25):
     current_loss = sys.maxsize
@@ -329,9 +330,10 @@ for col in range(25):
         if  temp_loss < current_loss:
             thresholds[col] = threshold
             current_loss = temp_loss
-            counts[col] = sum(pred_col > threshold)
-            y_trues[col] = sum(y_col > threshold)
-            # print(col, current_loss, thresholds[col], counts[col], y_trues[col])
+            counts[col] = sum(pred_col > threshold) / len(pred_col)
+            y_trues[col] = sum(y_col) / len(y_col)
+            print(col, current_loss, thresholds[col], counts[col], y_trues[col])
+
 
 print(thresholds)
 print(counts)
