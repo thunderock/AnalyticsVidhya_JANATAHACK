98ae6aed1bc0af5a250bcb50e1468f6d04a0911f
Ashutosh Tiwari <ashutosh113163@nitp.ac.in>
2020-11-13 22:51:20 +0530

    generating tsfresh features

--- stocks/second.py
+++ stocks/second.py
@@ -17,18 +17,22 @@ result_file = 'result.csv'
 df = pd.read_csv(train_file, low_memory=False, parse_dates=['Date'])
 
 
+
 def do_stock(dframe):
     unpredictability_score = dframe['unpredictability_score'].unique()[0]
     stock = dframe['stock'].unique()[0]
-    df_extra = extract_features(dframe.drop(columns=['stock', 'unpredictability_score', 'Close', 'train']), column_id = 'ID',
+    df_extra = extract_features(dframe.drop(columns=['stock', 'unpredictability_score', 'Close', 'High', 'Low', 'Open']), column_id = 'ID',
                             column_sort='Date', show_warnings=False, impute_function=impute, disable_progressbar=True,
                             n_jobs=0)
 
     df_extra['stock'] = stock
     df_extra['unpredictability_score'] = unpredictability_score
-    dframe.set_index('ID', drop=False, inplace=True)
+    # dframe.set_index('ID', drop=False, inplace=True)
     df_extra['Date'] = dframe['Date']
     df_extra['Close'] = dframe['Close']
+    df_extra['High'] = dframe['High']
+    df_extra['Low'] = dframe['Low']
+    df_extra['Open'] = dframe['Open']
     return df_extra
 
 num_cores = 7
@@ -37,6 +41,7 @@ num_cores = 7
 # rows = Parallel(n_jobs=num_cores)(delayed(do_stock)(df[df['stock'] == i]) for i in tqdm([1], total=1))
 rows = Parallel(n_jobs=num_cores)(delayed(do_stock)(df[df['stock'] == i]) for i in tqdm(df.stock.unique(), total=len(df.stock.unique())))
 
+del df
 df_extra = pd.concat(rows, axis=0)
 
 
